e5746a8d0a81a513167b342af17164272930c651
DESKTOP-L8PSM9T\alfio <fefferico81@gmail.com>
2021-11-08 09:24:02 +0900

    typo edit

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -16,7 +16,7 @@ export default function RecipeList({ recipes }) {
       {recipes.map(recipe => (
         <div key={recipe.id} className={`card ${mode}`}>
           <h3>{recipe.title}</h3>
-          <p>{recipe.cookingTime} to make.</p>
+          <p>{recipe.cookingTime} to cook.</p>
           <div>{recipe.method.substring(0, 100)}...</div>
           <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
         </div>
